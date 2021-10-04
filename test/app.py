1f1f1cc8abf537eaa2e9ad12fa88f9b450189d55
jie <eugene605110@gmail.com>
2021-10-04 17:00:48 +0800

    1004

--- test/app.py
+++ test/app.py
@@ -1,5 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def SID(p1, p2):
+    #ril = (apple.reshape(x*y,z)).transpose()
+    m = p1 / (np.sum(p1,axis=0))
+    n = p2 / (np.sum(p2,axis=0))
+    
+    D_rd = m * (np.log(m / n))
+    D_rd = np.sum(D_rd,axis=0)
+    D_dr = n * (np.log(n / m))
+    D_dr = np.sum(D_dr,axis=0)
+    
+    SID_result = np.double(D_rd + D_dr)
+    #SID_result = SID_rd.reshape(x,y)
+    
+    
+    return SID_result
+
+
+
+
+
 fp = r'panel.npy'
 data = np.load(fp,allow_pickle=True)
 item = data.item()
@@ -8,24 +30,28 @@ him = np.array(item.get('HIM'),'double')
 
 
 plt.figure()
-plt.imshow(him[:,:,100])
+plt.imshow(him[:,:,0])
 
 
 plt.figure()
 plt.imshow(gt)
 
-# plt.figure('www')
-# plt.title('title')
-# plt.xlabel('x')
-# plt.ylabel('y')
+plt.figure('www')
+plt.title('title')
+plt.xlabel('x')
+plt.ylabel('y')
 
-# '''for i in range(10):
-#     plt.plot(him[0,i,:])'''
+'''for i in range(10):
+    plt.plot(him[0,i,:])'''
     
 
-# arr = np.where(gt == 1)
-# print(arr)
+arr = np.where(gt == 1)
+print(arr)
 
-# for i in range(len(arr[0])):
-#     plt.plot(him[arr[0][i],arr[1][i],:])
-    # print(arr[0][i],arr[1][i])
+for i in range(len(arr[0])):
+    plt.plot(him[arr[0][i],arr[1][i],:])
+    print(arr[0][i],arr[1][i])
+    
+
+a = SID(him[7,37,:],him[20, 35, :] )
+print(a)
--- test/confer.py
+++ test/confer.py
@@ -2,17 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 ''' SAM '''
 def SAM(original, target):
-    x, y, z = original.shape#取得照片寬度，高度，維度
+    x, y, z = original.shape
     
-    B = (original.reshape(x * y, z)).transpose()#將矩陣轉置
+    B = (original.reshape(x * y, z)).transpose()
     
-    inner_ori_target = (B * target).sum(axis=0)#將基準向量與待測點向量內稽
-    norm_ori = np.power(np.power(B, 2).sum(axis=0), 0.5)#計算基準值的向量 
-    norm_target = np.power(np.power(target, 2).sum(axis=0), 0.5)#計算待測點的向量
-    x2 = inner_ori_target / (norm_ori * norm_target)#公式( <r*d>/|r|*|d| ) =x2   
-    dr = np.arccos(abs(x2))#將數值arccos(x2)
+    inner_ori_target = (B * target).sum(axis=0)
+    norm_ori = np.power(np.power(B, 2).sum(axis=0), 0.5)
+    norm_target = np.power(np.power(target, 2).sum(axis=0), 0.5)
+    x2 = inner_ori_target / (norm_ori * norm_target)   
+    dr = np.arccos(abs(x2))
     
-    SAM_result = 1 - dr.reshape(x, y)#將取得的數值轉成相片矩陣
+    SAM_result = 1 - dr.reshape(x, y)
     
     return SAM_result
 
