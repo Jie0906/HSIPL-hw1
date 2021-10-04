aab39b7c791201bf64b809f411af30e4538e86bb
Jie0906 <64222089+Jie0906@users.noreply.github.com>
2021-10-05 04:37:00 +0800

    Add files via upload

--- test/app.py
+++ test/app.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 
 def SID(p1, p2):
-    #ril = (apple.reshape(x*y,z)).transpose()
     m = p1 / (np.sum(p1,axis=0))
     n = p2 / (np.sum(p2,axis=0))
     
@@ -13,44 +12,33 @@ def SID(p1, p2):
     D_dr = np.sum(D_dr,axis=0)
     
     SID_result = np.double(D_rd + D_dr)
-    #SID_result = SID_rd.reshape(x,y)
-    
     
     return SID_result
 
 
-
-
+'''main'''
 
 fp = r'panel.npy'
 data = np.load(fp,allow_pickle=True)
 item = data.item()
-gt = item['groundtruth']
+gt = np.array(item.get('groundtruth'), 'double')
 him = np.array(item.get('HIM'),'double')
 
 
-plt.figure()
-plt.imshow(him[:,:,0])
+# plt.figure()
+# plt.imshow(him[:,:,0])
 
 
 plt.figure()
 plt.imshow(gt)
 
-plt.figure('www')
-plt.title('title')
-plt.xlabel('x')
-plt.ylabel('y')
-
-'''for i in range(10):
-    plt.plot(him[0,i,:])'''
-    
 
 arr = np.where(gt == 1)
 print(arr)
-
-for i in range(len(arr[0])):
-    plt.plot(him[arr[0][i],arr[1][i],:])
-    print(arr[0][i],arr[1][i])
+#plt.figure()
+# for i in range(len(arr[0])):
+#     plt.plot(him[arr[0][i],arr[1][i],:])
+#     print(arr[0][i],arr[1][i])
     
 
 a = SID(him[7,37,:],him[20, 35, :] )
--- test/sam.py
+++ test/sam.py
@@ -1,41 +1,57 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def SAM(apple,d_point):  
-    #print(x,y,z)
-    arr = []
-    rd = rtemp = dtemp = 0 # rd = <rd> 
-    for i in range(x):  
-        for j in range(y) :
-            for k in range(z) :
-                rd+=apple[i,j,k] * d_point[k]#rd做疊加
+# def SAM(apple,d_point):  
+#     #print(x,y,z)
+#     arr = []
+#     rd = rtemp = dtemp = 0 # rd = <rd> 
+#     for i in range(x):  
+#         for j in range(y) :
+#             for k in range(z) :
+#                 rd+=apple[i,j,k] * d_point[k]#rd做疊加
 
-                rtemp+=pow((apple[i,j,k]), 2)
-                r = pow(rtemp,0.5)
-                dtemp+=pow(d_point[k], 2)
-                d = pow(dtemp,0.5)
-                sam_rd = (rd/(r*d))   
-            arr.append(sam_rd)
-            rd = rtemp = dtemp = 0 
+#                 rtemp+=pow((apple[i,j,k]), 2)
+#                 r = pow(rtemp,0.5)
+#                 dtemp+=pow(d_point[k], 2)
+#                 d = pow(dtemp,0.5)
+#                 sam_rd = (rd/(r*d))   
+#             arr.append(sam_rd)
+#             rd = rtemp = dtemp = 0 
             
-    img = np.arccos(arr)
-    SAM_result = img.reshape(x,y)
+#     img = np.arccos(arr)
+#     SAM_result = img.reshape(x,y)
+#   
+#     return  SAM_result
 
+def SAM(apple, d_point):
+    r = (apple.reshape(x * y, z)).T
+    rd = np.sum((r * d_point), axis=0)
+    r_abs = (np.sum((r ** 2), axis=0)) ** 2 
+    d_abs =(np.sum((d_point ** 2), axis=0)) ** 2
+    #rd = (r * d_point).sum(axis=0) #<r,d>
+    #r_abs = ((r ** 2).sum(axis=0)) ** 2 #||r||
+    #d_abs =((d_point ** 2).sum(axis=0)) ** 2#||d||
+    temp = rd / (r_abs * d_abs)   
+    SAM_rd = np.arccos(temp)
     
-    return  SAM_result
+    SAM_result = SAM_rd.reshape(x, y)
+    
+    return SAM_result
+
+
 
 
 ''' main '''
 apple = plt.imread('apple.jpg')
 plt.imshow(apple)
-d_point = plt.ginput()
-
-apple = np.array(apple,dtype = "d")
+apple = np.double(apple)
 x,y,z = apple.shape#取得x,y,z的值
+
+d_point = plt.ginput()
 d_point = apple[int(d_point[0][1]),int(d_point[0][0]),:]
 plt.close()
 
-result = SAM(apple,d_point)#進行運算
+result = SAM(apple, d_point)#進行運算
 #print(result)
 
 
