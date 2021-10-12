a513876cb8e1e0f0e068b3fb100e20f66f1ee6d0
Jie0906 <64222089+Jie0906@users.noreply.github.com>
2021-10-12 18:13:55 +0800

    Add files via upload

--- test/hsiplHW1.py
+++ test/hsiplHW1.py
@@ -28,7 +28,25 @@ def SID(x, y):
     
     return SID_result
 
+def rsdpw_sam(x, y, target):
+    x_target_sam = SAM(x, target)
+    y_target_sam = SAM(y, target)
+    return max(x_target_sam/y_target_sam, y_target_sam/x_target_sam)
 
+def rsdpw_sid(x, y, target):
+    x_target_sid = SID(x, target)
+    y_target_sid = SID(y, target)
+    return max(x_target_sid/y_target_sid, y_target_sid/x_target_sid)
+
+def rsdpw_sid_tan(x, y, target):
+    x_target_sid_tan = SID(x, target) * np.tan(SAM(x, target))
+    y_target_sid_tan = SID(y, target) * np.tan(SAM(y, target))
+    return max(x_target_sid_tan/y_target_sid_tan, y_target_sid_tan/x_target_sid_tan)
+
+def rsdpw_sid_sin(x, y, target):
+    x_target_sid_sin = SID(x, target) * np.sin(SAM(x, target))
+    y_target_sid_sin = SID(y, target) * np.sin(SAM(y, target))
+    return max(x_target_sid_sin/y_target_sid_sin, y_target_sid_sin/x_target_sid_sin)
 
 '''main'''
 
@@ -69,33 +87,18 @@ SAM_p1p4 = SAM(him[7, 37, :], him[47, 33, :])
 SAM_p1p5 = SAM(him[7, 37, :], him[59, 33, :])
 print(SAM_p1p2, SAM_p1p3, SAM_p1p4, SAM_p1p5)
 
-SAM_p2p1 = SAM(him[20, 35, :], him[7, 37, :])
 SAM_p2p3 = SAM(him[20, 35, :], him[34, 34, :])
 SAM_p2p4 = SAM(him[20, 35, :], him[47, 33, :])
 SAM_p2p5 = SAM(him[20, 35, :], him[59, 33, :])
-print(SAM_p2p1, SAM_p2p3, SAM_p2p4, SAM_p2p5)
+print(SAM_p2p3, SAM_p2p4, SAM_p2p5)
 
-SAM_p3p1 = SAM(him[34, 34, :], him[7, 37, :])
-SAM_p3p2 = SAM(him[34, 34, :], him[20, 35, :])
 SAM_p3p4 = SAM(him[34, 34, :], him[47, 33, :])
 SAM_p3p5 = SAM(him[34, 34, :], him[59, 33, :])
-print(SAM_p3p1, SAM_p3p2, SAM_p3p4, SAM_p3p5)
+print(SAM_p3p4, SAM_p3p5)
 
-SAM_p4p1 = SAM(him[47, 33, :], him[7, 37, :])
-SAM_p4p2 = SAM(him[47, 33, :], him[20, 35, :])
-SAM_p4p3 = SAM(him[47, 33, :], him[34, 34, :])
 SAM_p4p5 = SAM(him[47, 33, :], him[59, 33, :])
-print(SAM_p4p1, SAM_p4p2, SAM_p4p3, SAM_p4p5)
-
-SAM_p5p1 = SAM(him[59, 33, :], him[7, 37, :])
-SAM_p5p2 = SAM(him[59, 33, :], him[20, 35, :])
-SAM_p5p3 = SAM(him[59, 33, :], him[34, 34, :])
-SAM_p5p4 = SAM(him[59, 33, :], him[47, 33, :])
-print(SAM_p5p1, SAM_p5p2, SAM_p5p3, SAM_p5p4)
-
-
-
-
+print(SAM_p4p5)
+print('-------------------------------------')
 
 ''''(2)_SID'''
 SID_p1p2 = SID(him[7, 37, :], him[20, 35, :])
@@ -104,26 +107,114 @@ SID_p1p4 = SID(him[7, 37, :], him[47, 33, :])
 SID_p1p5 = SID(him[7, 37, :], him[59, 33, :])
 print(SID_p1p2, SID_p1p3, SID_p1p4, SID_p1p5)
 
-SID_p2p1 = SID(him[20, 35, :], him[7, 37, :])
 SID_p2p3 = SID(him[20, 35, :], him[34, 34, :])
 SID_p2p4 = SID(him[20, 35, :], him[47, 33, :])
 SID_p2p5 = SID(him[20, 35, :], him[59, 33, :])
-print(SID_p2p1, SID_p2p3, SID_p2p4, SID_p2p5)
+print(SID_p2p3, SID_p2p4, SID_p2p5)
 
-SID_p3p1 = SID(him[34, 34, :], him[7, 37, :])
-SID_p3p2 = SID(him[34, 34, :], him[20, 35, :])
 SID_p3p4 = SID(him[34, 34, :], him[47, 33, :])
 SID_p3p5 = SID(him[34, 34, :], him[59, 33, :])
-print(SID_p3p1, SID_p3p2, SID_p3p4, SID_p3p5)
+print(SID_p3p4, SID_p3p5)
 
-SID_p4p1 = SID(him[47, 33, :], him[7, 37, :])
-SID_p4p2 = SID(him[47, 33, :], him[20, 35, :])
-SID_p4p3 = SID(him[47, 33, :], him[34, 34, :])
 SID_p4p5 = SID(him[47, 33, :], him[59, 33, :])
-print(SID_p4p1, SID_p4p2, SID_p4p3, SID_p4p5)
+print( SID_p4p5)
+print('-------------------------------------')
 
-SID_p5p1 = SID(him[59, 33, :], him[7, 37, :])
-SID_p5p2 = SID(him[59, 33, :], him[20, 35, :])
-SID_p5p3 = SID(him[59, 33, :], him[34, 34, :])
-SID_p5p4 = SID(him[59, 33, :], him[47, 33, :])
-print(SID_p5p1, SID_p5p2, SID_p5p3, SID_p5p4)
+''''(3)_SID_sin'''
+SID_p1p2_sin = SID_p1p2 * np.sin(SAM_p1p2)
+SID_p1p3_sin = SID_p1p3 * np.sin(SAM_p1p3)
+SID_p1p4_sin = SID_p1p4 * np.sin(SAM_p1p4)
+SID_p1p5_sin = SID_p1p5 * np.sin(SAM_p1p5)
+print(SID_p1p2_sin, SID_p1p3_sin, SID_p1p4_sin, SID_p1p5_sin)
+
+
+SID_p2p3_sin = SID_p2p3 * np.sin(SAM_p2p3)
+SID_p2p4_sin = SID_p2p4 * np.sin(SAM_p2p4)
+SID_p2p5_sin = SID_p2p5 * np.sin(SAM_p2p5)
+print(SID_p2p3_sin, SID_p2p4_sin, SID_p2p5_sin)
+
+
+SID_p3p4_sin = SID_p3p4 * np.sin(SAM_p3p4)
+SID_p3p5_sin = SID_p3p5 * np.sin(SAM_p3p5)
+print(SID_p3p4_sin, SID_p3p5_sin)
+
+SID_p4p5_sin = SID_p4p5 * np.sin(SAM_p4p5)
+print(SID_p4p5_sin)
+print('----------------------------------')
+
+''''(3)_SID_tan'''
+SID_p1p2_tan = SID_p1p2 * np.tan(SAM_p1p2)
+SID_p1p3_tan = SID_p1p3 * np.sin(SAM_p1p3)
+SID_p1p4_tan = SID_p1p4 * np.sin(SAM_p1p4)
+SID_p1p5_tan = SID_p1p5 * np.sin(SAM_p1p5)
+print(SID_p1p2_tan, SID_p1p3_tan, SID_p1p4_tan, SID_p1p5_tan)
+
+
+SID_p2p3_tan = SID_p2p3 * np.tan(SAM_p2p3)
+SID_p2p4_tan = SID_p2p4 * np.tan(SAM_p2p4)
+SID_p2p5_tan = SID_p2p5 * np.tan(SAM_p2p5)
+print(SID_p2p3_tan, SID_p2p4_tan, SID_p2p5_tan)
+
+SID_p3p4_tan = SID_p3p4 * np.tan(SAM_p3p4)
+SID_p3p5_tan = SID_p3p5 * np.tan(SAM_p3p5)
+print(SID_p3p4_tan, SID_p3p5_tan)
+
+
+SID_p4p5_tan = SID_p4p5 * np.tan(SAM_p4p5)
+print(SID_p4p5_tan)
+print('----------------------------------')
+
+''''(4)_RSDPW_SAM'''
+RSDPW_SAM_p1p2 = rsdpw_sam(him[7, 37, :], him[20, 35, :], him[34, 34, :])
+RSDPW_SAM_p1p4 = rsdpw_sam(him[7, 37, :], him[47, 33, :], him[34, 34, :])
+RSDPW_SAM_p1p5 = rsdpw_sam(him[7, 37, :], him[59, 33, :], him[34, 34, :])
+print(RSDPW_SAM_p1p2, RSDPW_SAM_p1p4, RSDPW_SAM_p1p5)
+
+RSDPW_SAM_p2p4 = rsdpw_sam(him[20, 35, :], him[47, 33, :], him[34, 34, :])
+RSDPW_SAM_p2p5 = rsdpw_sam(him[20, 35, :], him[59, 33, :], him[34, 34, :])
+print(RSDPW_SAM_p2p4, RSDPW_SAM_p2p5)
+
+RSDPW_SAM_p4p5 = rsdpw_sam(him[47, 33, :], him[59, 33, :], him[34, 34, :])
+print(RSDPW_SAM_p4p5)
+print('----------------------------------')
+''''(4)_RSDPW_SID'''
+RSDPW_SID_p1p2 = rsdpw_sid(him[7, 37, :], him[20, 35, :], him[34, 34, :])
+RSDPW_SID_p1p4 = rsdpw_sid(him[7, 37, :], him[47, 33, :], him[34, 34, :])
+RSDPW_SID_p1p5 = rsdpw_sid(him[7, 37, :], him[59, 33, :], him[34, 34, :])
+print(RSDPW_SID_p1p2, RSDPW_SID_p1p4, RSDPW_SID_p1p5)
+
+RSDPW_SID_p2p4 = rsdpw_sid(him[20, 35, :], him[47, 33, :], him[34, 34, :])
+RSDPW_SID_p2p5 = rsdpw_sid(him[20, 35, :], him[59, 33, :], him[34, 34, :])
+print(RSDPW_SID_p2p4, RSDPW_SID_p2p5)
+
+RSDPW_SID_p4p5 = rsdpw_sid(him[47, 33, :], him[59, 33, :], him[34, 34, :])
+print(RSDPW_SID_p4p5)
+print('----------------------------------')
+
+''''RSDPW_SID_tan'''
+RSDPW_SID_p1p2_tan = rsdpw_sid_tan(him[7, 37, :], him[20, 35, :], him[34, 34, :])
+RSDPW_SID_p1p4_tan = rsdpw_sid_tan(him[7, 37, :], him[47, 33, :], him[34, 34, :])
+RSDPW_SID_p1p5_tan = rsdpw_sid_tan(him[7, 37, :], him[59, 33, :], him[34, 34, :])
+print(RSDPW_SID_p1p2_tan, RSDPW_SID_p1p4_tan, RSDPW_SID_p1p5_tan)
+
+RSDPW_SID_p2p4_tan = rsdpw_sid_tan(him[20, 35, :], him[47, 33, :], him[34, 34, :])
+RSDPW_SID_p2p5_tan = rsdpw_sid_tan(him[20, 35, :], him[59, 33, :], him[34, 34, :])
+print(RSDPW_SID_p2p4_tan, RSDPW_SID_p2p5_tan)
+
+RSDPW_SID_p4p5_tan = rsdpw_sid_tan(him[47, 33, :], him[59, 33, :], him[34, 34, :])
+print(RSDPW_SID_p4p5_tan)
+print('----------------------------------')
+
+''''RSDPW_SID_sin'''
+RSDPW_SID_p1p2_sin = rsdpw_sid_sin(him[7, 37, :], him[20, 35, :], him[34, 34, :])
+RSDPW_SID_p1p4_sin = rsdpw_sid_sin(him[7, 37, :], him[47, 33, :], him[34, 34, :])
+RSDPW_SID_p1p5_sin = rsdpw_sid_sin(him[7, 37, :], him[59, 33, :], him[34, 34, :])
+print(RSDPW_SID_p1p2_sin, RSDPW_SID_p1p4_sin, RSDPW_SID_p1p5_sin)
+
+RSDPW_SID_p2p4_sin = rsdpw_sid_sin(him[20, 35, :], him[47, 33, :], him[34, 34, :])
+RSDPW_SID_p2p5_sin = rsdpw_sid_sin(him[20, 35, :], him[59, 33, :], him[34, 34, :])
+print(RSDPW_SID_p2p4_sin, RSDPW_SID_p2p5_sin)
+
+RSDPW_SID_p4p5_sin = rsdpw_sid_sin(him[47, 33, :], him[59, 33, :], him[34, 34, :])
+print(RSDPW_SID_p4p5_sin)
+print('----------------------------------')
